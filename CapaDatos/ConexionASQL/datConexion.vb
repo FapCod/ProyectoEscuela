0274bee7b9baaac1dcc28738e0d896c799e0a0b1
frank piñin ato <frankantoniopiato@gmail.com>
2019-10-20 18:51:08 -0500

    datos vacios

--- CapaDatos/ConexionASQL/datConexion.vb
+++ CapaDatos/ConexionASQL/datConexion.vb
@@ -1,18 +1,4 @@
 ﻿Imports System.Data.SqlClient
 Public Class datConexion
-    Public cnn As New SqlConnection
-
-    Public Function ConectarSinUsuario() As Boolean
-        Dim cadena As String
-        cadena = "Data Source=FAPCOD\SQLEXPRESS;Initial Catalog=Colegio_DB"
-        Try
-            cnn = New SqlConnection(cadena)
-            cnn.Open()
-            Return True
-        Catch ex As Exception
-            MsgBox(ex.Message)
-            Return False
-
-        End Try
-    End Function
+    
 End Class
